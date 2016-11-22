5d51158231938bdd766cda61cde0d029b7d505a8
Adrien Destugues <pulkomandy@gmail.com>
2016-11-22 21:30:23 +0000

    Show total number of frames in animation bar.
    Fixes #11.
    
    
    git-svn-id: svn://pulkomandy.tk/GrafX2/trunk@2169 416bcca6-2ee7-4201-b75f-2eb2f807beb1

--- src/windows.c
+++ src/windows.c
@@ -586,18 +586,18 @@ void Display_layerbar(void)
   }
   if (Menu_bars[MENUBAR_ANIMATION].Visible)
   {
-    char str[8];
+    char str[9];
     // Rest of horizontal line
     Draw_bar_remainder(MENUBAR_ANIMATION, Menu_bars[MENUBAR_ANIMATION].Skin_width);
     // Frame# background rectangle
     // Block((Menu_bars[MENUBAR_ANIMATION].Skin_width)*Menu_factor_X,(0+Menu_bars[MENUBAR_ANIMATION].Top)*Menu_factor_Y+Menu_Y,8*8*Menu_factor_X,8*Menu_factor_Y,MC_Light);
     // Frame #/#
-    snprintf(str, 5, "#%3d", Main_current_layer+1);
-    Print_general((82)*Menu_factor_X,(Menu_bars[MENUBAR_ANIMATION].Top+3)*Menu_factor_Y+Menu_Y,str,MC_Black,MC_Light);
+    snprintf(str, 8, "%3d/%3d", Main_current_layer+1, Main_backups->Pages->Nb_layers);
+    Print_general((59)*Menu_factor_X,(Menu_bars[MENUBAR_ANIMATION].Top+3)*Menu_factor_Y+Menu_Y,str,MC_Black,MC_Light);
     Update_rect(
-      (82)*Menu_factor_X,
+      (59)*Menu_factor_X,
       (Menu_bars[MENUBAR_ANIMATION].Top+3)*Menu_factor_Y+Menu_Y,
-      4*8*Menu_factor_X, 
+      7*8*Menu_factor_X, 
       8*Menu_factor_Y);
   }
 }
